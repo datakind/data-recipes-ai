64d6a1f0f758425cdf5720e23a50a9dc682e9a01
Matthew Harris <matthew@regolith.org>
2024-05-05 16:21:15 -0400

    Better naming

--- cleanup.sh
+++ cleanup.sh
@@ -4,4 +4,4 @@ rm -rf ./ui/recipes_assistant_chat/images/*
 rm -rf ./ui/recipes_assistant_chat/logs/*
 rm -rf ./ui/recipes_assistant_chat/data-node/*
 rm -rf ./ui/recipes_assistant_chat/meili_data_v1.7/*
-rm -rf ./ui/recipes_assistant_chat/pgdata2/*
+rm -rf ./ui/recipes_assistant_chat/datadb/*
