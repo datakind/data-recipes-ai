86a4de26f74a863fe9f66032991ab48fbda8566f
Matthew Harris <matthew@regolith.org>
2024-06-08 19:03:28 -0400

    Major Refactor for simplicity and to remove Librechat in favor of chainlit for easier product packaging.

--- server/fastapi/app.py
+++ server/fastapi/app.py
@@ -1,19 +1,71 @@
+import os
 import sys
 
-from actions import get_memory_recipe
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from utils.recipes import get_memory_recipe
+from utils.utils import execute_query as utils_execute_query
+
 
 class MemoryRecipeInput(BaseModel):
+    """
+    Represents the input data for a memory recipe.
+
+    Attributes:
+        user_input (str): The user's input.
+        chat_history (str): The chat history.
+        generate_intent (str, optional): Whether to generate an intent. Defaults to "false".
+    """
+
     user_input: str
     chat_history: str
-    generate_intent: str = "true"
+    generate_intent: str = "false"
+
+
+class ExecuteQueryInput(BaseModel):
+    """
+    Represents the input data for executing a query.
+
+    Attributes:
+        query (str): The query to be executed.
+    """
+
+    query: str
 
 
 app = FastAPI()
 
 
 @app.post("/get_memory_recipe")
-def memory_recipe(data: MemoryRecipeInput):
+def memory_recipe_route(data: MemoryRecipeInput):
+    """
+    Retrieves a memory recipe based on the provided input data.
+
+    Args:
+        data (MemoryRecipeInput): The input data containing user input, chat history, and generate intent.
+
+    Returns:
+        The memory recipe generated based on the input data.
+    """
     return get_memory_recipe(data.user_input, data.chat_history, data.generate_intent)
+
+
+@app.post("/execute_query")
+def execute_query_route(data: ExecuteQueryInput):
+    """
+    Executes a query and returns the results.
+
+    Args:
+        data (ExecuteQueryInput): The input data containing the query.
+
+    Returns:
+        pandas.DataFrame: The results of the query.
+    """
+    results = utils_execute_query(data.query)
+
+    # TODO: Add code to send back a link, in case results are too large
+    if results.shape[0] > 500:
+        print("Results are too large to send back")
+
+    return results
