97793a5a343db332d7a37c12c06555ec4c0e8ffb
Matthew Harris <matthew@regolith.org>
2024-06-08 14:03:44 -0400

    Major Refactor for simplicity and to remove Librechat in favor of chainlit for easier product packaging.

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,3 @@
 docker compose down -v
-
-rm -rf ./ui/recipes-chat/images/*
-rm -rf ./ui/recipes-chat/logs/*
-rm -rf ./ui/recipes-chat/data-node/*
-rm -rf ./ui/recipes-chat/meili_data_v1.7/*
-rm -rf ./ui/recipes-chat/datadb/*
-rm -rf ./ui/recipes-chat/recipesdb/*
+rm -rf ./data/datadb/*
+rm -rf ./data/recipesdb/*
---
+++ server/fastapi/app.py
@@ -0,0 +1,19 @@
+import sys
+
+from actions import get_memory_recipe, run_recipe
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class MemoryRecipeInput(BaseModel):
+    user_input: str
+    chat_history: str
+    generate_intent: str = "true"
+
+
+app = FastAPI()
+
+
+@app.post("/get_memory_recipe")
+def memory_recipe(data: MemoryRecipeInput):
+    return get_memory_recipe(data.user_input, data.chat_history, data.generate_intent)
