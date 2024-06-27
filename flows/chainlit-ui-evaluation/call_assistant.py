9b97c4805714903d8487a0111cec01a3b03229b5
Matthew Harris <matthew@regolith.org>
2024-06-27 17:24:17 -0400

    Interim commit, still having thread management issues due to async chainlit

--- flows/chainlit-ui-evaluation/call_assistant.py
+++ flows/chainlit-ui-evaluation/call_assistant.py
@@ -96,6 +96,7 @@ def setup_mock_class():
                 MockMessage: The updated message.
             """
             print(f"Streaming token: Author: {self.author}, Content: {content}")
+            self.content += content
             return self
 
         async def update(self):
@@ -222,7 +223,7 @@ def run_sync(func, *args, **kwargs):
         return func(*args, **kwargs)
 
 
-def test():
+async def test_using_app_code():
     """
     This function is used to test the functionality of the app module.
     It sets up a mock chainlit class, imports the app module, and overrides certain methods and event handlers.
@@ -238,15 +239,11 @@ def test():
     # Override run_sync method to use mock cl
     app.run_sync = run_sync
 
-    # Override OpenAI event handler to use mock cl
-    event_handler = app.get_event_handler(cl_mock, "assistant")
-    cl_mock.user_session.set("event_handler", event_handler)
-
     # Patch 'cl' with our Mock class
     app.cl = cl_mock
 
     # Call app start to set up variables
-    asyncio.run(app.start_chat())
+    await app.start_chat()
 
     # Here insert history to thread
     # thread_id = app.cl.user_session.get("thread_id")
@@ -254,8 +251,75 @@ def test():
 
     # msg = cl_mock.Message(author="You", content="What is the total population of Mali", elements=[])
     msg = cl_mock.Message(author="You", content="Hi", elements=[])
-    asyncio.run(app.main(msg))
+    await app.process_message(msg)
+
+    # Get last message
+    # messages = await app.async_openai_client.beta.threads.messages.list(thread_id)
+    # print(messages)
+    # print(messages.data[0].content[0].text.value)
+
+    print("Closing OpenAI thread")
+    # app.sync_openai_client.beta.threads.delete(thread_id)
+
+
+def test_using_local_code():
+    """
+    This function is used to test the functionality of the app module.
+    It sets up a mock chainlit class, imports the app module, and overrides certain methods and event handlers.
+    Then it calls the app start and main functions to simulate a chat interaction.
+    """
+
+    # Create a Mock chainlit class
+    cl_mock = setup_mock_class()
+
+    # Import chainlit app
+    import app as app
+
+    # Override run_sync method to use mock cl
+    app.run_sync = run_sync
+
+    # Patch 'cl' with our Mock class
+    app.cl = cl_mock
+
+    # Call app start to set up variables
+    thread = asyncio.run(app.async_openai_client.beta.threads.create())
+
+    # Here insert history to thread
+    # thread_id = app.cl.user_session.get("thread_id")
+    # wait app.add_message_to_thread(thread_id, "user", message.content, message)
+
+    # msg = cl_mock.Message(author="You", content="What is the total population of Mali", elements=[])
+    message = cl_mock.Message(author="You", content="Hi", elements=[])
+
+    print(f"Adding user message {message.content} to thread {thread.id}")
+    app.sync_openai_client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=message.content,
+    )
+
+    event_handler = app.get_event_handler(cl_mock, "Assistant")
+    with app.sync_openai_client.beta.threads.runs.stream(
+        thread_id=thread.id,
+        assistant_id=app.assistant.id,
+        event_handler=event_handler,
+    ) as stream:
+        stream.until_done()
+
+    # Get last thread message
+    messages = app.sync_openai_client.beta.threads.messages.list(thread.id)
+    print(messages.data[0].content[0].text.value)
+
+    print("Closing OpenAI thread")
+    app.sync_openai_client.beta.threads.delete(thread.id)
+
+    return
+
+    # First check memories
 
 
 if __name__ == "__main__":
-    test()
+    # This works, but doesn't exist due to async stuff, likely in chainlit.
+    # asyncio.run(test_using_app_code())
+
+    test_using_local_code()
