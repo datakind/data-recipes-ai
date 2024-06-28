177d3504ac21fb399685db56180810072798366f
Matthew Harris <matthew@regolith.org>
2024-06-28 11:19:30 -0400

    Implemented workaround for async hanging thread, to call script and kill after execution. Obviously, this is a very hacky workaround to be able to use the exact chainlit code for e2e tests, and we may not use this (we're implementing unit tests for recipes server and assistant independantly), but will finish implementation as it works and nearly done

--- flows/chainlit-ui-evaluation/call_assistant.py
+++ flows/chainlit-ui-evaluation/call_assistant.py
@@ -1,12 +1,17 @@
+import argparse
 import asyncio
 import inspect
-import sys
+import json
+import os
+import signal
+import subprocess
 import threading
-from contextlib import asynccontextmanager
-from contextvars import ContextVar
+import time
 
-import chainlit as cl
 from promptflow.core import tool
+
+FINISH_PHRASE = "all done"
+OUTPUT_TAG = "ASSISTANT_OUTPUT"
 
 
 @tool
@@ -21,13 +26,15 @@ def call_assistant(query: str) -> dict:
         [
             {
                 "author": "user",
-                "content": "Hi",
-                "elements": []
+                "content": "Hi"
             },
             {
                 "author": "assistant",
                 "content": "Hello! How can I help you today?",
-                "elements": []
+            },
+            {
+                "author": "assistant",
+                "content": "What's the total population of Mali?",
             }
         ]
 
@@ -35,9 +42,23 @@ def call_assistant(query: str) -> dict:
         dict: A dictionary containing the response from the assistant, function name, function arguments,
               function output, and the number of tokens in the function output.
     """
-    message = "Hi"
 
-    result = test_using_app_code(message)
+    chat_history = [
+        {"author": "user", "content": "Hi"},
+        {
+            "author": "assistant",
+            "content": "Hello! How can I help you today?",
+        },
+        {
+            "author": "assistant",
+            "content": "Hi again!",
+        },
+    ]
+    chat_history = json.dumps(chat_history)
+    chat_history = chat_history.replace('"', '\\"')
+    chat_history = chat_history.replace("'", "\\'")
+
+    result = run_chainlit_mock(chat_history)
 
     response = {"response": result}
 
@@ -164,6 +185,18 @@ def setup_mock_class():
             self.__name__ = "chainlit"
             self.step = None
 
+        def Text(self, name, content, display):
+            """
+            Creates a text element.
+
+            Args:
+                text (str): The text content.
+
+            Returns:
+                dict: A dictionary containing the text element.
+            """
+            return {"type": "Text", "text": content}
+
     cl_mock = MockChainlit()
 
     return cl_mock
@@ -200,6 +233,54 @@ def run_async_coroutine(coroutine):
         return None
 
 
+def run_chainlit_mock(chat_history: str) -> str:
+    """
+    This function is used to run the chainlit script and monitor its output.
+    TODO It is a temporary workaround because running the exact chainlit code
+    does not exit all asynchronous threads and hangs. This workaround is temporary
+    and should be replaced by breaking e2e testing into data recipes API and
+    the assistant. Testing both independently is way less complicated.
+
+    Args:
+        chat_history (str): A string containing the chat history.
+
+    Returns:
+        result (str): The result of the chainlit script running with input history
+
+    """
+
+    all_output = ""
+    result = ""
+    print("Monitoring chainlit output")
+    process = subprocess.Popen(
+        ["python3", "call_assistant.py", "--chat_history", chat_history],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print(process)
+    while True:
+        output = process.stdout.readline()
+        print(output)
+        if output == b"" and process.poll() is not None:
+            break
+        if output:
+            all_output += output.decode("utf-8")
+            print(output.strip())
+            if FINISH_PHRASE in str(output).lower():
+                print(FINISH_PHRASE)
+                print("Killing process")
+                os.kill(process.pid, signal.SIGKILL)
+                print(OUTPUT_TAG)
+                if OUTPUT_TAG in all_output:
+                    result = all_output.split(OUTPUT_TAG)[1].strip()
+                    print("Result:", result)
+                else:
+                    result = "Unparsable output"
+                break
+        time.sleep(0.1)
+    return result
+
+
 def run_sync(func, *args, **kwargs):
     """
     Run a function synchronously or asynchronously depending on its type.
@@ -228,49 +309,82 @@ def run_sync(func, *args, **kwargs):
         return func(*args, **kwargs)
 
 
-def test_using_app_code(msg):
-    """
-    This function is used to test the functionality of the app module.
-    It sets up a mock chainlit class, imports the app module, and overrides certain methods and event handlers.
-    Then it calls the app start and main functions to simulate a chat interaction.
-    """
+async def test_using_app_code_async(msg, timeout=5):
 
-    # Create a Mock chainlit class
     cl_mock = setup_mock_class()
-
-    # Import chainlit app
     import app as app
 
-    # Override run_sync method to use mock cl
     app.run_sync = run_sync
-
-    # Patch 'cl' with our Mock class
     app.cl = cl_mock
 
-    # Call app start to set up variables
-    asyncio.run(app.start_chat())
+    await app.start_chat()
 
-    # Here insert history to thread
     thread_id = app.cl.user_session.get("thread_id")
-    # wait app.add_message_to_thread(thread_id, "user", message.content, message)
 
-    # msg = cl_mock.Message(author="You", content="What is the total population of Mali", elements=[])
     msg = cl_mock.Message(author="You", content=msg, elements=[])
-    asyncio.run(app.process_message(msg))
+    await app.process_message(msg)
 
-    # Get last message
     messages = app.sync_openai_client.beta.threads.messages.list(thread_id)
     result = messages.data[0].content[0].text.value
-    print(messages.data[0].content[0].text.value)
-
-    print("Closing OpenAI thread")
-    app.sync_openai_client.beta.threads.delete(thread_id)
-
-    print("All done")
 
     return result
 
 
+def test_using_app_code(msg):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(test_using_app_code_async(msg))
+    loop.close()
+    return result
+
+
+def main_direct_function():
+    """
+    TODO
+    For testing direct function call, which hangs even though finished because of
+    some issue with async. Left here for future reference for somebody to fix so
+    the script execution and kill hack can be retired.
+
+    """
+    # chat_history = '[{\"author\": \"user\",\"content\": \"Hi\"},{\"author\":\"assistant\content\": \"Hello! How can I help you today?\"},{\"author\": \"assistant\",\"content\": \"What is the total population of Mali?\"}]'
+    chat_history = '[{"author": "user","content": "Hi"}'
+
+    result = test_using_app_code(chat_history)
+    print("OUTPUT")
+    print(result)
+    print("OUTPUT")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Process check in and check out operations (i.e. extracting recipes and recipes from the database for quality checks and edits)."
+    )
+
+    parser.add_argument(
+        "--chat_history",
+        type=str,
+        required=True,
+        help="""
+            A list containing the chat history, of the format (but in one line) ...
+
+            '[{\"author\": \"user\",\"content\": \"Hi\"},{\"author\":\"assistant\",\"content\": \"Hello! How can I help you today?\"},{\"author\": \"assistant\",\"content\": \"What is the total population of Mali?\"}]'
+        """,
+    )
+
+    args = parser.parse_args()
+    chat_history = args.chat_history
+
+    if chat_history:
+        result = test_using_app_code(chat_history)
+        print(OUTPUT_TAG)
+        print(result)
+        print(OUTPUT_TAG)
+
+        # Do not remove this line
+        print(FINISH_PHRASE)
+
+
 if __name__ == "__main__":
-    # This works, but doesn't exist due to async stuff, likely in chainlit.
-    test_using_app_code("Hi")
+    # main_direct_function()
+    main()
