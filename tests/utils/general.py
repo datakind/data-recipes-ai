6784605ed0267bc9e350d3b8587b8fde48123e6e
JanPeterDatakind <Jan.Peter@Datakind.org>
2024-07-05 15:23:08 -0400

    temporary fix to isolate the error

---
+++ tests/utils/general.py
@@ -0,0 +1,153 @@
+import base64
+import json
+import os
+import re
+import sys
+import warnings
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+execute_query_url = f"{os.getenv('RECIPE_SERVER_API')}execute_query"
+get_memory_recipe_url = f"{os.getenv('RECIPE_SERVER_API')}get_memory_recipe"
+
+data_info = None
+
+
+def replace_env_variables(value):
+    """
+    Recursively replaces environment variable placeholders in a given value.
+
+    Args:
+        value (dict, list, str): The value to process
+
+    Returns:
+        The processed value with environment variable placeholders replaced.
+
+    """
+    if isinstance(value, dict):
+        return {k: replace_env_variables(v) for k, v in value.items()}
+    elif isinstance(value, list):
+        return [replace_env_variables(v) for v in value]
+    elif isinstance(value, str):
+        matches = re.findall(r"\{\{ (.+?) \}\}", value)
+        for match in matches:
+            if os.getenv(match) is None:
+                print(f"Environment variable {match} is not set.")
+                sys.exit(1)
+
+            value = value.replace("{{ " + match + " }}", os.getenv(match))
+        return value
+    else:
+        return value
+
+
+def read_integration_config(integration_config_file):
+    """
+    Read the APIs configuration from the integration config file.
+
+    Args:
+        integration_config_file (str): The path to the integration config file.
+
+    Returns:
+        apis (dict): A dictionary containing the API configurations.
+        field_map (dict): A dictionary containing the field mappings.
+        standard_names (dict): A dictionary containing the standard names.
+        data_node (str): The data node to use for the integration.
+    """
+    with open(integration_config_file) as f:
+        print(f"Reading {integration_config_file}")
+        config = json.load(f)
+        config = replace_env_variables(config)
+        apis = config["openapi_interfaces"]
+        field_map = config["field_map"]
+        standard_names = config["standard_names"]
+
+    return apis, field_map, standard_names
+
+
+def is_running_in_docker():
+    """
+    Check if the code is running inside a Docker container.
+
+    Returns:
+        bool: True if running inside a Docker container, False otherwise.
+    """
+    return os.path.exists("/.dockerenv")
+
+
+def make_api_request(url, payload):
+    """
+    Makes an API request to the specified URL with the given payload.
+
+    Args:
+        url (str): The URL to make the API request to.
+        payload (dict): The payload to send with the API request.
+
+    Returns:
+        dict: The response from the API as a dictionary.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs while making the API request.
+    """
+    headers = {"Content-Type": "application/json"}
+    print(f"API URL: {url}")
+    print(f"API Payload: {payload}")
+    response = requests.post(url, headers=headers, json=payload)
+    print(f"API Response Status Code: {response.status_code}")
+    response = response.content
+    print(f"API Response {response}")
+    return response
+
+
+def call_execute_query_api(sql):
+    """
+    Calls the execute query action API endpoint with the given SQL query.
+
+    Args:
+        sql (str): The SQL query to execute.
+
+    Returns:
+        dict: The response from the API.
+
+    """
+    data = {"query": f"{sql}"}
+    print(f"Calling execute query API {execute_query_url} with {sql} ...")
+    return make_api_request(execute_query_url, data)
+
+
+def call_get_memory_recipe_api(user_input, history, generate_intent="true"):
+    """
+    Calls the API to get a memory recipe action.
+
+    Args:
+        user_input (str): The user input.
+        history (str): The chat history.
+        generate_intent (str): Whether to generate the intent.
+
+
+    Returns:
+        The API response from the make_api_request function.
+    """
+
+    data = {
+        "user_input": f"{user_input}",
+        "chat_history": history,
+        "generate_intent": "true",
+    }
+    print(f"Calling execute query API {get_memory_recipe_url} with {data} ...")
+    result = make_api_request(get_memory_recipe_url, data)
+
+    if isinstance(result, bytes):
+        result = result.decode("utf-8")
+
+    print("IN API CALL", result)
+    result = json.loads(result)
+
+    return result
---
+++ tests/utils/llm.py
@@ -0,0 +1,213 @@
+import base64
+import json
+import os
+import sys
+
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader
+from langchain.schema import HumanMessage, SystemMessage
+from langchain_openai import (
+    AzureChatOpenAI,
+    AzureOpenAIEmbeddings,
+    ChatOpenAI,
+    OpenAIEmbeddings,
+)
+
+from utils.db import get_data_info
+
+load_dotenv()
+
+# Caps for LLM summarization of SQL output and number of rows in the output
+llm_prompt_cap = 5000
+sql_rows_cap = 100
+
+# Because CLI runs on host. TODO, make this more elegant.
+template_dir = "../templates"
+if not os.path.exists(template_dir):
+    template_dir = "./templates"
+environment = Environment(loader=FileSystemLoader(template_dir))
+sql_prompt_template = environment.get_template("gen_sql_prompt.jinja2")
+
+chat = None
+embedding_model = None
+
+data_info = None
+
+
+def get_models():
+    """
+    Retrieves the embedding model and chat model based on the specified API type.
+
+    Returns:
+        embedding_model: The embedding model used for text embeddings.
+        chat: The chat model used for generating responses.
+
+    Raises:
+        SystemExit: If the specified API type is not supported.
+    """
+    api_key = os.getenv("RECIPES_OPENAI_API_KEY")
+    base_url = os.getenv("RECIPES_BASE_URL")
+    api_version = os.getenv("RECIPES_OPENAI_API_VERSION")
+    api_type = os.getenv("RECIPES_OPENAI_API_TYPE")
+    completion_model = os.getenv("RECIPES_OPENAI_TEXT_COMPLETION_DEPLOYMENT_NAME")
+    model = os.getenv("RECIPES_MODEL")
+    temp = os.getenv("RECIPES_MODEL_TEMP")
+    max_tokens = os.getenv("RECIPES_MODEL_MAX_TOKENS")
+
+    if api_type == "openai":
+        embedding_model = OpenAIEmbeddings(
+            api_key=api_key,
+        )
+        chat = ChatOpenAI(
+            model_name=model,
+            api_key=api_key,
+            temperature=temp,
+            max_tokens=max_tokens,
+        )
+    elif api_type == "azure":
+        embedding_model = AzureOpenAIEmbeddings(
+            api_key=api_key,
+            deployment=completion_model,
+            azure_endpoint=base_url,
+            chunk_size=16,
+        )
+        chat = AzureChatOpenAI(
+            api_key=api_key,
+            api_version=api_version,
+            azure_endpoint=base_url,
+            model_name=model,
+            temperature=temp,
+            max_tokens=max_tokens,
+        )
+    else:
+        print(f"OPENAI API type: {api_type} not supported")
+        sys.exit(1)
+    return embedding_model, chat
+
+
+def call_llm(instructions, prompt, image=None):
+    """
+    Call the LLM (Language Learning Model) API with the given instructions and prompt.
+
+    Args:
+        instructions (str): The instructions to provide to the LLM API.
+        prompt (str): The prompt to provide to the LLM API.
+        chat (Langchain Open AI model): Chat model used for AI judging
+
+    Returns:
+        dict or None: The response from the LLM API as a dictionary, or None if an error occurred.
+    """
+
+    global chat, embedding_model
+    if chat is None or embedding_model is None:
+        embedding_model, chat = get_models()
+
+    human_message = HumanMessage(content=prompt)
+
+    # Multimodal
+    if image:
+        if os.getenv("RECIPES_MODEL") == "gpt-4o":
+            print("Sending image to LLM ...")
+            with open(image, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode()
+
+            human_message = HumanMessage(
+                content=[
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{encoded_string}"
+                        },
+                    },
+                ]
+            )
+        else:
+            print("Multimodal not supported for this model")
+            return None
+
+    try:
+        messages = [
+            SystemMessage(content=instructions),
+            human_message,
+        ]
+        response = chat(messages)
+
+        if hasattr(response, "content"):
+            response = response.content
+
+        if "content" in response and not response.startswith("```"):
+            response = json.loads(response)
+            response = response["content"]
+
+        # Some silly things that sometimes happen
+        response = response.replace(",}", "}")
+
+        # Different models do different things when prompted for JSON. Here we try and handle this
+        try:
+            # Is it already JSON?
+            response = json.loads(response)
+        except json.decoder.JSONDecodeError:
+            # Did the LLM provide JSON in ```json```?
+            if "```json" in response:
+                # print("LLM responded with JSON in ```json```")
+                response = response.split("```json")[1]
+                response = response.replace("\n", "").split("```")[0]
+                response = json.loads(response)
+            elif "```python" in response:
+                # print("LLM responded with Python in ```python```")
+                all_sections = response.split("```python")[1]
+                code = all_sections.replace("\n", "").split("```")[0]
+                message = all_sections.split("```")[0]
+                response = {}
+                response["code"] = code
+                response["message"] = message
+            else:
+                # Finally just send it back
+                print("LLM response unparsable, using raw results")
+                print(response)
+                response = {"content": response}
+        return response
+
+    except Exception as e:
+        # print(response)
+        print("Error calling LLM: ", e)
+        response = None
+
+
+async def gen_sql(input, chat_history, output):
+    """
+    Generate SQL query based on input, chat history, and output.
+
+    Args:
+        input (str): The input for generating the SQL query.
+        chat_history (str): The chat history used for generating the SQL query.
+        output (str): The output of the SQL query.
+
+    Returns:
+        str: The generated SQL query.
+
+    Raises:
+        None
+
+    """
+    global data_info
+
+    if data_info is None:
+        data_info = await get_data_info()
+
+    prompt = sql_prompt_template.render(
+        input=input,
+        stdout_output=output,
+        stderr_output="",
+        data_info=data_info,
+        chat_history=chat_history,
+    )
+
+    response = call_llm("", prompt)
+
+    query = response["code"]
+
+    query = query.replace(";", "") + f" \nLIMIT {sql_rows_cap};"
+
+    return query
